b7cff6c9fd303061f06d60b6667bd288e746b89a
chupengdai <71621552+chupengdai@users.noreply.github.com>
2020-09-21 11:58:45 +0800

    Update cdt_edt.sql

--- cdt_edt.sql
+++ cdt_edt.sql
@@ -1,4 +1,4 @@
-SELECT allorders.orderid
+SELECT OD.orderid
 ,json_extract(orderdatatab.extra_data, '$.delivery_detail_info.edt_min_dt') as EDT_min
 ,json_extract(orderdatatab.extra_data, '$.delivery_detail_info.edt_max_dt') as EDT_max
 ,json_extract(orderdatatab.extra_data, '$.delivery_detail_info.apt') as seller_apt
@@ -6,6 +6,6 @@ SELECT allorders.orderid
 ,json_extract(orderdatatab.extra_data, '$.delivery_detail_info.cdt_max') as CDT_max
 ,json_extract(orderdatatab.extra_data, '$.delivery_detail_info.he_pt') as APT_extended
 ,json_extract(orderdatatab.extra_data, '$.delivery_detail_info.he_cdt') as CDT_extended
-FROM shopee_order_v4_db__order_v4_tab allorders
+FROM shopee_order_v4_db__order_v4_tab OD
 LEFT JOIN shopee_order_processing_backend_PH_db__order_ext_data_tab orderdatatab -- change country
-ON allorders.extinfo.order_ext_data_ref = orderdatatab.order_ext_data_ref
+ON OD.extinfo.order_ext_data_ref = orderdatatab.order_ext_data_ref
